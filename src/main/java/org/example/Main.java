6f114449bccf71fbc4d8b19e5e8a08e19aac2f25
Polina Kuhmar <polina.kuhmar@vrpconsulting.com>
2025-09-20 13:01:00 +0300

    Домашнее задание №1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String developerLastName = "Кухмар";
+        String dateOfTask = "10/09/2025";
+        String timeOfTask = "21:03";
+        System.out.println("Фамилия разработчика: " + developerLastName);
+        System.out.println("Дата и время получения задания: " + dateOfTask + " "  + timeOfTask);
     }
 }
